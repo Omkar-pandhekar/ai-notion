0a6b1e229bd34d8aec8e6d2e25d91acc3f692fc1
Omkar-pandhekar <141856931+Omkar-pandhekar@users.noreply.github.com>
2025-08-19 16:09:42 +0530

    last time

--- src/app/auth-endpoint/route.ts
+++ src/app/auth-endpoint/route.ts
@@ -1,13 +1,14 @@
 import liveblocks from "@/lib/liveblocks";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
-import { adminDb } from "../../../firebase-admin";
+import { getAdminDb } from "../../../firebase-admin";
 
 export async function POST(req: NextRequest) {
   auth.protect();
 
   const { sessionClaims } = await auth();
   const { room } = await req.json();
+  const adminDb = getAdminDb();
 
   if (!sessionClaims?.sub || typeof room !== "string" || !room) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
